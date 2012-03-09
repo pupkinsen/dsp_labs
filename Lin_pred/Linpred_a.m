6298cd81feffa35b75c780f7488ad73495433812
unknown <Uz0r@PC01.(none)>
2012-03-09 13:16:43 +0400

    новые функции

---
+++ Lin_pred/Linpred_a.m
@@ -0,0 +1,30 @@
+﻿function out = Linpred_a(x,P)
+%Вывод коэффициентов линейного предсказания
+M=mean(x);
+N=length(x);
+%Коэффициенты корреляции (автокореляционный метод)
+p=N;
+for k=0:p
+    s=0;
+    for m=1:(N-k)
+        s=s+(x(m)-M)*(x(m+k)-M);
+    end;
+    RR(k+1)=s;
+end;
+clear k s m
+for k=1:(p+1)
+    R(k)=RR(k)/RR(1);
+end;
+clear k RR
+
+
+
+p=P; %Синтез для порядка p
+    alfa=zeros(1,p);
+%        for i=1:p
+%            alfa(i)=al(p,i);
+%        end
+    zz=levinson(R,p);
+    alfa=-zz(1,2:end);
+
+        out=alfa;
---
+++ Lin_pred/Linpred_sint.m
@@ -0,0 +1,17 @@
+﻿function out = Linpred_sint(x,nsig,alfa)
+
+
+p=length(alfa); %Синтез для порядка p
+    
+
+    s=zeros(nsig,1);
+    s(1:p)=x(1:p);
+        for k=p+1:length(s)
+            for l=1:p
+                if(k-l>=1)
+                s(k)=s(k)+alfa(l)*s(k-l);
+                end
+            end
+      
+        end
+        out=s;
